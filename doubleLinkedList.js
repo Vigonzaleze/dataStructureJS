c17f6c9798bdda9ddb5065d15fed6e4f49d36b15
Vicente Gonzalez <johndoe@example.com>
2020-10-26 17:01:50 -0300

    lista doblemente enlazada lista

---
+++ doubleLinkedList.js
@@ -0,0 +1,184 @@
+class Node{
+    constructor(data, next, prev){
+        this.data = data,
+        this.next = next, 
+        this.prev = prev
+    }
+}
+
+class DoubleLinkedList{
+    constructor(){
+        this.head = null
+        this.tail = null
+        this.size = 0
+    }
+
+    print(){
+        let current = this.head
+        let result = ''
+        while(current){
+            result += current.data + ' <-> '
+            current = current.next
+        }
+
+        return result += ' X '
+    }
+
+    reversePrint(){
+        let current = this.tail
+        let result = ''
+        while(current){
+            result += current.data + ' <-> '
+            current = current.prev
+        }
+
+        return result + ' X '
+    }
+
+    addtoHead(data){
+        const newNode = new Node(data, this.head, null)
+        
+        if(this.head){
+            newNode.next = this.head
+            this.head.prev = newNode
+            this.head = newNode
+        } else {
+            this.head = newNode
+            this.tail = newNode
+        }
+        this.size++
+    }
+
+    addtoTail(data){
+        const newNode = new Node(data, null, this.tail)
+
+        if(this.tail){
+            newNode.prev = this.tail
+            this.tail.next = newNode
+            this.tail = newNode
+        } else {
+            this.tail = newNode
+            this.head = newNode
+        }
+        this.size++
+    }
+
+    insertAt(data, index){
+        if(index < 0 || index > this.size){
+            return null
+        }
+
+        const newNode = new Node(data, null, null)
+        let current = this.head
+        let previous
+
+        if(index === 0){
+            newNode.next = current
+            current.prev = newNode
+            this.head = newNode
+        } else {
+            for (let i = 0; i < index; i++){
+                previous = current
+                current = current.next
+            }
+
+            newNode.next = current
+            newNode.prev = previous
+            current.prev = newNode
+            previous.next = newNode
+        }
+        this.size++
+    }
+
+    removeFromHead(){
+        if(!this.head){
+            return null
+        }
+
+        const valueToReturn = this.head.data
+
+        if(this.head === this.tail){
+            this.head = null
+            this.tail = null
+        } else {
+            this.head = this.head.next
+            this.head.prev = null
+        }
+        this.size--
+        return valueToReturn
+    }
+
+    removeFromTail(){
+        if(!this.tail){
+            return null
+        }
+
+        const valueToReturn = this.tail.data
+
+        if(this.head === this.tail){
+            this.head = null
+            this.tail = null
+        } else {
+            this.tail = this.tail.prev
+            this.tail.next = null
+        }
+        this.size--
+        return valueToReturn
+    }
+
+    removeData(data){
+        let current = this.head
+        let previous = null
+
+        while(current !== null){
+            if(current.data === data){
+                if(!previous){
+                    return this.removeFromHead()
+                } else if (!current.next) {
+                    return this.removeFromTail()
+                } else {
+                    previous.next = current.next
+                    current.next.prev = previous
+                }
+                this.size--
+                return current.data
+            }
+            previous = current
+            current = current.next
+        }
+        return null
+    }
+
+    getSize(){
+        return this.size
+    }
+    
+    isEmpty(){
+        return this.size === 0
+    }
+
+}
+
+const doubleLinkedList = new DoubleLinkedList()
+
+doubleLinkedList.addtoHead(3)
+doubleLinkedList.addtoHead(4)
+doubleLinkedList.addtoTail(6)
+doubleLinkedList.addtoHead(5)
+doubleLinkedList.addtoTail(7)
+doubleLinkedList.addtoTail(8)
+doubleLinkedList.insertAt(9, 1)
+console.log(doubleLinkedList.print())
+console.log(doubleLinkedList.reversePrint())
+doubleLinkedList.removeFromHead()
+console.log(doubleLinkedList.print())
+console.log(doubleLinkedList.reversePrint())
+doubleLinkedList.removeFromTail()
+console.log(doubleLinkedList.print())
+console.log(doubleLinkedList.reversePrint())
+console.log(doubleLinkedList)
+doubleLinkedList.removeData(6)
+console.log(doubleLinkedList.print())
+console.log(doubleLinkedList.reversePrint())
+console.log(doubleLinkedList.getSize())
+console.log(doubleLinkedList.isEmpty())
